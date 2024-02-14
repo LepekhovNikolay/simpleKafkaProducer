b125b1884332321e780f01030dad2a3170b34117
VTB4096603 <nlepehov@vtb.ru>
2024-02-14 18:47:58 +0300

    changed file with settings to json format.

--- src/main/java/simplekafkaproducer/ui/Repository.java
+++ src/main/java/simplekafkaproducer/ui/Repository.java
@@ -1,102 +1,84 @@
 package simplekafkaproducer.ui;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.yaml.snakeyaml.LoaderOptions;
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor;
 import simplekafkaproducer.ui.dto.Setting;
 import simplekafkaproducer.ui.dto.Topic;
 
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Repository {
 
-	private Setting properties;
+    private Setting properties;
 
-	public Repository() {
-		try {
-			downloadProps();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+    public Repository() {
+        try {
+            downloadProps();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	public String getServerAddress() {
-		return properties.getServerAddress();
-	}
+    public String getServerAddress() {
+        return properties.getServerAddress();
+    }
 
-	public Collection<String> getTopics() {
-		return properties.getTopics().stream()
-				.map(Topic::getName)
-				.collect(Collectors.toList());
-	}
+    public Collection<String> getTopics() {
+        return properties.getTopics().stream().map(Topic::getName).collect(Collectors.toList());
+    }
 
-	public String getMessage(String topic) {
-		return properties.getTopics().stream()
-				.filter(t -> t.getName().equals(topic))
-				.map(Topic::getMessage)
-				.findAny()
-				.orElse("");
+    public String getMessage(String topic) {
+        return properties.getTopics().stream().filter(t -> t.getName().equals(topic)).map(Topic::getMessage).findAny().orElse("");
 
-	}
+    }
 
-	public Collection<String> getHeaders(String topic) {
-		return properties.getTopics().stream()
-				.filter(t -> t.getName().equals(topic))
-				.filter(t -> t.getHeaders() != null)
-				.map(Topic::getHeaders)
-				.findAny()
-				.orElse(new ArrayList<>());
-	}
+    public Collection<String> getHeaders(String topic) {
+        return properties.getTopics().stream().filter(t -> t.getName().equals(topic)).filter(t -> t.getHeaders() != null).map(Topic::getHeaders).findAny().orElse(new ArrayList<>());
+    }
 
-	private void downloadProps() throws IOException {
-		Yaml yaml = new Yaml(new CustomClassLoaderConstructor(
-				Setting.class,
-				ClassLoader.getSystemClassLoader(),
-				new LoaderOptions()));
-		properties = yaml.load(Files.newInputStream(Paths.get("settings.yaml")));
-	}
+    private void downloadProps() throws IOException {
+        File settingsAsFile = new File("settings.json");
+        if (!settingsAsFile.exists()) {
+            settingsAsFile.createNewFile();
+            Files.writeString(settingsAsFile.toPath(), "{}");
+        }
+        properties = new ObjectMapper().readValue(settingsAsFile, Setting.class);
 
-	public void updateTopicSettings(String topicName,
-									String message,
-									List<String> headers) {
-		properties.getTopics().stream()
-				.filter(t -> t.getName().equals(topicName))
-				.findAny()
-				.ifPresentOrElse(t -> {
-					t.setMessage(message);
-					t.setHeaders(headers);
-				}, () -> properties.getTopics().add(new Topic(topicName, message, headers)));
-		flushProperties();
-	}
+    }
 
-	public void removeCurrentTopic(String topicName) {
-		properties.getTopics().stream()
-				.filter(topic -> topic.getName().equals(topicName))
-				.findAny()
-				.ifPresent(topicToRemove -> {
-					properties.getTopics().remove(topicToRemove);
-					flushProperties();
-				});
-	}
+    public void updateTopicSettings(String topicName,
+                                    String message,
+                                    List<String> headers) {
+        properties.getTopics().stream().filter(t -> t.getName().equals(topicName)).findAny().ifPresentOrElse(t -> {
+            t.setMessage(message);
+            t.setHeaders(headers);
+        }, () -> {
+            properties.getTopics().add(new Topic(topicName, message, headers));
+        });
+        flushProperties();
+    }
 
-	private void flushProperties() {
-		try {
-			Yaml yaml = new Yaml();
-			FileWriter fileWriter = new FileWriter("settings.yaml");
-			Object propertiesAsMap = new ObjectMapper().convertValue(properties, Map.class);
-			yaml.dump(propertiesAsMap, fileWriter);
-			fileWriter.flush();
-		} catch (Exception e) {
-			throw new RuntimeException(e.getMessage(), e);
-		}
-	}
+    public void removeCurrentTopic(String topicName) {
+        properties.getTopics().stream().filter(topic -> topic.getName().equals(topicName)).findAny().ifPresent(topicToRemove -> {
+            properties.getTopics().remove(topicToRemove);
+            flushProperties();
+        });
+    }
+
+    private void flushProperties() {
+        try(PrintWriter pw = new PrintWriter(new FileWriter("settings.json"))) {
+            String propertiesAsString = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(properties);
+            pw.write(propertiesAsString);
+            pw.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/simplekafkaproducer/ui/dto/Setting.java
+++ src/main/java/simplekafkaproducer/ui/dto/Setting.java
@@ -2,13 +2,29 @@ package simplekafkaproducer.ui.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.commons.lang3.StringUtils;
 
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-@Getter
 @Setter
 public class Setting {
 	private String serverAddress;
 	private Set<Topic> topics;
+
+	public String getServerAddress() {
+		if (StringUtils.isBlank(serverAddress))
+			serverAddress = "localhost:9092";
+		return serverAddress;
+	}
+
+	public Set<Topic> getTopics() {
+		if (topics == null)
+			topics = new HashSet<>();
+		return topics;
+	}
 }
+
+
